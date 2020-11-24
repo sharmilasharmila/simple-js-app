aec8d1393fa8653a6cabb292501bb6c0fd5f2d06
sharmilasharmila <sra084@users.noreply.github.com>
2020-11-24 16:12:49 +0530

    Update scripts.js
    code added

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,12 @@
+<<<<<<< HEAD
 alert('Hello world');
 
 let food = 'Bob';
 document.write(food);
+=======
+alert("hello world")
+let myName = 'Sharmila';
+document.write(myName);
+favoriteFood = '  I like chocolate';
+document.write(favoriteFood);
+>>>>>>> ed15d2a9d7c8f1a3383f48fab0b3c4ab199dcac0
