f7b48aa050a80bee870f3e5e90670bc23b20906a
sharmilasharmila <73405006+sharmilasharmila@users.noreply.github.com>
2020-11-24 16:11:21 +0530

    Update scripts.js
    Objects are declared within the array

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,16 @@
-let pokemonList = [];
-  pokemonList[0]={
+let pokemonList = [{
     name:'balbasour',
     height:7,
     type:['grass','poison']
-    };
-  pokemonList[1]={
+    },
+  {
     name:'charmendor',
     height:6,
     type:['blaze','solar-power']
-    };
-  pokemonList[2]={
+    },
+  {
     name:'squirtle',
     height:5,
     type:['Rain-dish','torrent']
-    };
+  }
+];
