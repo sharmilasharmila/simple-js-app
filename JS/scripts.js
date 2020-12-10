b7f895b329a1c3a8d96570bb5802833341689fdb
sharmilasharmila <sra084@post.uit.no>
2020-12-11 00:27:15 +0500

    APIs, Ajax and Asynchronous Behavoir Task Ready for review

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,19 +1,6 @@
 let pokemonRepository = (function(){
-  let pokemonList = [{
-      name:'balbasour',
-      height:7,
-      type:['grass','poison']
-      },
-    {
-      name:'charmendor',
-      height:6,
-      type:['blaze','solar-power']
-      },
-    {
-      name:'squirtle',
-      height:5,
-      type:['Rain-dish','torrent']
-    }];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -37,18 +24,77 @@ let pokemonRepository = (function(){
 });
   }
   function showDetails(pokemon){
-	  console.log(pokemon);
+  loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+  });
+  }
+  
+  
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+	  showLoadingMessage();
+  }
+  
+  
+
+  function loadDetails(item) {
+	  showLoadingMessage();
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+		hideLoadingMessage();
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+		hideLoadingMessage();
+      console.error(e);
+    });
   }
 
   return {
     add: add,
     getAll: getAll,
-	addListItem:addListItem
+	addListItem:addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails  
   };
   
   
 }())
 
-pokemonRepository.getAll().forEach(function(item){
-	pokemonRepository.addListItem(item);
+
+pokemonRepository.loadList().then(function() {
+	hideLoadingMessage();  
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
+//pokemonRepository.getAll().forEach(function(item){
+//	pokemonRepository.addListItem(item);
+//});
+var sh_message=document.querySelector('#sh_message');
+function showLoadingMessage(){
+if (sh_message.style.display === "none") {
+    sh_message.style.display = "block";
+  } 
+}
+function hideLoadingMessage(){
+	if (sh_message.style.display === "block"){
+    sh_message.style.display = "none";
+  }	
+}
