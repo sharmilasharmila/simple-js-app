fadbf5909bbae8a06a28607e334f3d8e2d72030d
sharmilasharmila <sra084@users.noreply.github.com>
2020-12-22 18:06:47 +0530

    Update scripts.js
    prettier corrected after added some new corrections

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function() {
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -11,21 +11,21 @@ let pokemonRepository = (function() {
   }
 
   function addListItem(pokemon) {
-    var sh_ul = document.querySelector('.pokemon-list');
-    let sh_listItem = document.createElement('li');
-    let sh_button = document.createElement('button');
+    var sh_ul = document.querySelector(".pokemon-list");
+    let sh_listItem = document.createElement("li");
+    let sh_button = document.createElement("button");
     sh_button.innerText = pokemon.name;
-    sh_button.classList.add('sharmila-pokemon-button');
-    sh_button.classList.add('btn');
-    sh_button.classList.add('btn-info');
+    sh_button.classList.add("sharmila-pokemon-button");
+    sh_button.classList.add("btn");
+    sh_button.classList.add("btn-info");
 
-    sh_listItem.classList.add('group-list-item');
-    sh_button.setAttribute('data-toggle', 'modal'); //this works with bootstrap to open the modal when the pokemon name button is clicked
-    sh_button.setAttribute('data-target', '#pokemonModal');
+    sh_listItem.classList.add("group-list-item");
+    sh_button.setAttribute("data-toggle", "modal"); //this works with bootstrap to open the modal when the pokemon name button is clicked
+    sh_button.setAttribute("data-target", "#pokemonModal");
     sh_listItem.appendChild(sh_button);
     sh_ul.appendChild(sh_listItem);
     //
-    sh_button.addEventListener('click', function() {
+    sh_button.addEventListener("click", function() {
       showDetails(pokemon);
     });
   }
@@ -94,22 +94,22 @@ pokemonRepository.loadList().then(function() {
 
 //showModal function added
 function showModal(pokemon) {
-  let modalBody = $('.modal-body');
-  let modalTitle = $('.modal-title');
+  let modalBody = $(".modal-body");
+  let modalTitle = $(".modal-title");
 
   modalTitle.empty(); //empties anything that was previously stored in these variables
   modalBody.empty();
 
   //add the new content into the new "modal" div (Name, height, and image content)
   //Name element
-  let nameElement = $('<h1>' + pokemon.name + '</h1>');
+  let nameElement = $("<h1>" + pokemon.name + "</h1>");
 
   //Height content
-  let heightElement = $('<p>' + 'Height: ' + pokemon.height + '</p>');
+  let heightElement = $("<p>" + "Height: " + pokemon.height + "</p>");
 
   //Image content
   let imageElement = $('<img class="modal-img" style="width:50%">');
-  imageElement.attr('src', pokemon.imageUrl);
+  imageElement.attr("src", pokemon.imageUrl);
 
   //appends the children to their parent containers
   modalTitle.append(nameElement);
